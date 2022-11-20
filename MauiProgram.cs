6c6775bdf7a2f8e5365951b1819c861e164bf1b2
Pascal JK <pascual.santos80@gmail.com>
2022-11-21 00:57:44 +0200

    - added fonts to project.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,6 +11,12 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("DancingScript-Regular.ttf", "DancingScriptRegular");
+				fonts.AddFont("IndieFlower-Regular.ttf", "IndieFlowerRegular");
+				fonts.AddFont("Pacifico-Regular.ttf", "PacificoRegular");
+				fonts.AddFont("PlayfairDisplay-Regular.ttf", "PlayfairDisplayRegular");
+				fonts.AddFont("RubikDistressed-Regular.ttf", "RubikDistressedRegular");
+				fonts.AddFont("Ubuntu-Regular.ttf", "UbuntuRegular");
 				fonts.AddFont("materialdesignicons.ttf", "MDI");
 			});
 
