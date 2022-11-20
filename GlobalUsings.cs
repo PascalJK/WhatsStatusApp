f6aff7301fc30cf172f39a464d4c9ee4764f37fa
Pascal JK <pascual.santos80@gmail.com>
2022-11-20 20:44:40 +0200

    - added GestureRecognizer to change background color label in MainPage. - added StatusBackgroundColor logic. - fixed syntax error made in AppShell.xaml.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,3 +1,5 @@
 ﻿global using WhatsStatusApp.ViewModels;
+global using WhatsStatusApp.Views;
 global using CommunityToolkit.Mvvm;
 global using CommunityToolkit.Mvvm.ComponentModel;
+global using System.Windows.Input;
--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,5 +1,37 @@
 ﻿namespace WhatsStatusApp.ViewModels;
 internal partial class MainPageViewModel : BaseViewModel
 {
+    readonly List<Color> backgroudColors;
+    readonly Random random = new();
+
+    [ObservableProperty] bool _IsAllCaps;
     [ObservableProperty] string _StatusText;
+    [ObservableProperty] string _StatusFont;
+    [ObservableProperty] Color _StatusBackgroundColor;
+
+    public ICommand ChangeStatusBackgroundColorCommand => new Command(SetRandomStatusBackgroundColor);
+
+    public MainPageViewModel()
+    {
+        backgroudColors = LoadStatusBackgroundColors();
+        SetRandomStatusBackgroundColor();
+    }
+
+    static List<Color> LoadStatusBackgroundColors()
+    {
+        return new List<Color>()
+        {
+            Color.FromArgb("2980B9"),
+            Color.FromArgb("27AE60"),
+            Color.FromArgb("D35400"),
+            Color.FromArgb("2d3436"),
+            Color.FromArgb("FFC107"),
+            Color.FromArgb("FF4081"),
+            Color.FromArgb("2d3436"),
+            Color.FromArgb("512BD4"),
+        };
+    }
+
+    void SetRandomStatusBackgroundColor()
+        => StatusBackgroundColor = backgroudColors[random.Next(backgroudColors.Count)];
 }
