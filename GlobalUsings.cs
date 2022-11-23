c2db48d95008dd8e454666e8b88043d7d8f33b7f
Pascal JK <pascual.santos80@gmail.com>
2022-11-23 19:51:17 +0200

    - added Models, OpenGraphNet and Interfaces to global using directives.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,5 +1,8 @@
-﻿global using WhatsStatusApp.ViewModels;
+﻿global using WhatsStatusApp.Interfaces;
+global using WhatsStatusApp.Models;
+global using WhatsStatusApp.ViewModels;
 global using WhatsStatusApp.Views;
 global using CommunityToolkit.Mvvm;
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using System.Windows.Input;
+global using OpenGraphNet;
