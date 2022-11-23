03cc39fb432956603050927484444b76526f5f69
Pascal JK <pascual.santos80@gmail.com>
2022-11-23 22:06:51 +0200

    - updated TargetFrameworks from [net6] to [net7]

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using WhatsStatusApp.Interfaces;
 global using WhatsStatusApp.Models;
+global using WhatsStatusApp.Services;
 global using WhatsStatusApp.ViewModels;
 global using WhatsStatusApp.Views;
 global using CommunityToolkit.Mvvm;
