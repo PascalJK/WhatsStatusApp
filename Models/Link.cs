508c83de886a4c0d8681cdb36af690989af7f1d9
Pascal JK <pascual.santos80@gmail.com>
2022-11-23 20:02:08 +0200

    - created a Links Model class. - added needed properties to class. - added Method to link DecodeMetaInformation passed from OpenGraph.

---
+++ Models/Link.cs
@@ -0,0 +1,40 @@
+﻿using OpenGraphNet.Metadata;
+
+namespace WhatsStatusApp.Models;
+
+public class Link
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public string URL { get; set; }
+    public bool Found
+    {
+        get
+        {
+            return !string.IsNullOrEmpty(Title) || !string.IsNullOrEmpty(Description) || !string.IsNullOrEmpty(Image);
+        }
+    }
+
+    readonly string noDescription = @"no description";
+    readonly string defaultImage = @"https://img.icons8.com/emoji/80/000000/link-emoji.png";
+
+    public Link DecodeMetaInformation(OpenGraph og)
+    {
+        URL = og.OriginalUrl.OriginalString;
+
+        Title = og.Metadata["og:title"].Value();
+        if(string.IsNullOrWhiteSpace(Title))
+            Title = og.Metadata["og:site_name"].Value();
+
+        Description = og.Metadata["og:description"].Value();
+        if(string.IsNullOrWhiteSpace(Description))
+            Description = noDescription;
+
+        Image = og.Metadata["og:image"].Value();
+        if(string.IsNullOrWhiteSpace(Image))
+            Image = defaultImage;
+        return this;
+
+    }
+}
