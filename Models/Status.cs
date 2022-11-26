d948373c0bbfdf12ff5e46f303f8f868142a02f1
Pascal JK <pascual.santos80@gmail.com>
2022-11-26 18:38:57 +0200

    - set Id field as primarykey. - added a Color property. - other quality made.

--- Models/Status.cs
+++ Models/Status.cs
@@ -1,6 +1,7 @@
 ﻿namespace WhatsStatusApp.Models;
 internal class Status
 {
+    [PrimaryKey]
     public string Id { get; set; }
     public string Text { get; set; }
     public string ColorHex { get; set; }
@@ -8,15 +9,21 @@ internal class Status
     public string Font { get; set; }
     public DateTime DateCreated { get; set; }
 
+    [Ignore]
+    public Color Color { get; private set; }
+
     public Status CreateNewStatusModel(MainPageViewModel vm)
     {
         Id = ShortGuid.NewGuid().ToString();
         Text = vm.StatusText;
-        ColorHex = vm.StatusBackgroundColor.ToHex();
+        ColorHex = vm.StatusBackgroundColor.ToArgbHex();
         TextTransform = vm.StatusTextTransform;
         TextTransform = vm.StatusTextTransform;
         Font = vm.StatusFont;
         DateCreated = DateTime.Now;
         return this;
     }
+
+    public void SetStatusBackGroundColor()
+        => Color = Color.FromArgb(ColorHex);
 }
