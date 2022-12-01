fe7c477032c70ae3fab0e517650a31aad846997b
Pascal JK <pascual.santos80@gmail.com>
2022-12-01 11:39:17 +0200

    - [Updates] made changes to Styles ResourceDictionary. - worked on app styling on pages and views. - [new] added controlTemplate inside TemplateDictionary. - added TextTemplate class.

---
+++ Resources/Templates/TemplateDictionary.xaml.cs
@@ -0,0 +1,9 @@
+namespace WhatsStatusApp.Resources.Templates;
+
+public partial class TemplateDictionary : ResourceDictionary
+{
+	public TemplateDictionary()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Resources/Templates/TextTemplate.cs
@@ -0,0 +1,18 @@
+﻿namespace WhatsStatusApp.Resources.Templates;
+
+public class TextTemplate : ContentView
+{
+    public static readonly BindableProperty IconGlyphProperty = BindableProperty.Create(nameof(IconGlyph), typeof(string), typeof(TextTemplate), string.Empty);
+    public static readonly BindableProperty LabelTextProperty = BindableProperty.Create(nameof(LabelText), typeof(string), typeof(TextTemplate), string.Empty);
+
+    public string IconGlyph
+    {
+        get => (string)GetValue(IconGlyphProperty);
+        set => SetValue(IconGlyphProperty, value);
+    }
+    public string LabelText
+    {
+        get => (string)GetValue(LabelTextProperty);
+        set => SetValue(LabelTextProperty, value);
+    }
+}
